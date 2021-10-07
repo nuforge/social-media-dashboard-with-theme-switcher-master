34ffca395980fc50d79789f12b6ef8d5c763f485
nuforge <nuforge@gmail.com>
2021-10-07 10:14:03 -0400

    Toggle dark mode javascript

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,10 @@
-console.log("Hello");
+const radioButtons = document.querySelectorAll('.toggle__wrapper input');
+
+for (var i = 0 ; i < radioButtons.length; i++) {
+    radioButtons[i].addEventListener('click', (event) => {
+        document.getElementById('dark').checked 
+        ? (document.querySelector('body').classList = 'dark') 
+        : (document.querySelector('body').classList = 'light');
+
+    });
+}
--- dist/script.js
+++ dist/script.js
@@ -1,2 +1,2 @@
-"use strict";console.log("Hello");
+"use strict";for(var radioButtons=document.querySelectorAll(".toggle__wrapper input"),i=0;i<radioButtons.length;i++)radioButtons[i].addEventListener("click",(function(t){document.getElementById("dark").checked?document.querySelector("body").classList="dark":document.querySelector("body").classList="light"}));
 //# sourceMappingURL=script.js.map
