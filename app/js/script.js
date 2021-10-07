fe1561c6dff46288597050e3b43608c07c09ff84
nuforge <nuforge@gmail.com>
2021-10-07 14:56:40 -0400

    Tutorial Complete: Local Storage

--- app/js/script.js
+++ app/js/script.js
@@ -1,10 +1,54 @@
 const radioButtons = document.querySelectorAll('.toggle__wrapper input');
+const darkButton = document.getElementById('dark');
+const lightButton = document.getElementById('light');
+
+
+const setColorMode = () => {
+    if( localStorage.getItem('colorMode') == 'dark') {
+        darkButton.click();
+        setDarkMode();
+    } else if( localStorage.getItem('colorMode') == 'light'){
+        lightButton.click();
+        setLightMode(); 
+    }
+}
+
+const checkMode = () => {
+    if(window.matchMedia("(prefers-color-scheme: light)").matches) {
+        lightButton.click();
+    } else if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+        darkButton.click();
+    }
+}
+
+const checkModeChange = () => {
+    window.matchMedia('(prefers-color-scheme: dark)')
+    .addEventListener('change', (event) => {
+        checkMode();
+    })
+}
+
+const setDarkMode = () => {
+    document.querySelector('body').classList = 'dark';
+}
+
+const setLightMode = () => {
+    document.querySelector('body').classList = 'light';
+}
+
+setColorMode();
+checkMode();
+checkModeChange();
 
 for (var i = 0 ; i < radioButtons.length; i++) {
     radioButtons[i].addEventListener('click', (event) => {
-        document.getElementById('dark').checked 
-        ? (document.querySelector('body').classList = 'dark') 
-        : (document.querySelector('body').classList = 'light');
+        if(darkButton.checked) {
+            localStorage.setItem('colorMode', 'dark');
+            setDarkMode();
+        } else {
+            localStorage.setItem('colorMode', 'light');
+            setLightMode();
+        }
 
     });
 }
